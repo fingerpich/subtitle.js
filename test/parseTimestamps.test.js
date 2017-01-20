a0ef0f492c3642ccfbe412debfa15669b6654add
Guilherme Santiago <guilherme.santiago95@gmail.com>
2017-01-20 14:02:45 -0200

    Improve `parseTimestamps` tests

--- test/parseTimestamps.test.js
+++ test/parseTimestamps.test.js
@@ -10,13 +10,35 @@ const { parseTimestamps } = require('../lib/utils')
  */
 
 test('should parse the timestamps', t => {
-  const str = '00:00:50,000 --> 00:00:53,000'
-  const value = parseTimestamps(str)
-  const expected = {
-    start: '00:00:50,000',
-    end: '00:00:53,000',
-    duration: 3000
-  }
+  const data = [
+    {
+      srtTime: '00:00:50,000 --> 00:00:53,000',
+      expected: {
+        start: '00:00:50,000',
+        end: '00:00:53,000',
+        duration: 3000
+      }
+    },
+    {
+      srtTime: '00:02:22,542 --> 00:02:25,284',
+      expected: {
+        start: '00:02:22,542',
+        end: '00:02:25,284',
+        duration: 2742
+      }
+    },
+    {
+      srtTime: '02:13:01,305 --> 02:13:07,267',
+      expected: {
+        start: '02:13:01,305',
+        end: '02:13:07,267',
+        duration: 5962
+      }
+    }
+  ]
 
-  t.deepEqual(value, expected)
+  data.forEach(obj => {
+    const value = parseTimestamps(obj.srtTime)
+    t.deepEqual(value, obj.expected)
+  })
 })
